f02009e1bf13f61026906efcd1103d29f9551246
Michael <michael.feduk@gmail.com>
2023-10-06 12:03:32 +0000

    added new page

---
+++ pages/5_DataFrame_Test.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+QUERY = """
+SELECT * FROM `elegant-tendril-395105.1.cleaned_jobs`
+LIMIT 100
+"""
+
+# Fetch data from BigQuery
+def fetch_data(query):
+    return pd.read_gbq(query, project_id="your_project_id", dialect="standard", use_bqstorage_api=True)
+
+# Use Streamlit to display the data
+st.write(fetch_data(QUERY))
