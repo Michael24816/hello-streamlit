c987d9194ab7c611d4238a2aedaea3a8b856f1cc
Michael <michael.feduk@gmail.com>
2023-10-06 12:51:23 +0000

    try again

--- pages/5_DataFrame_Test.py
+++ pages/5_DataFrame_Test.py
@@ -1,8 +1,6 @@
 import streamlit as st
-from google.cloud import bigquery
-
-# Initialize a BigQuery client without specifying credentials
-client = bigquery.Client(project="elegant-tendril-395105", credentials=None)
+import requests
+import pandas as pd
 
 # Define the SQL query
 QUERY = """
@@ -10,9 +8,23 @@ SELECT * FROM `elegant-tendril-395105.1.cleaned_jobs`
 LIMIT 100
 """
 
-# Fetch data from BigQuery
+# Fetch data from BigQuery using REST API
 def fetch_data(query):
-    return client.query(query).to_dataframe()
+    url = f"https://bigquery.googleapis.com/bigquery/v2/projects/elegant-tendril-395105/queries"
+    data = {
+        "query": query,
+        "useLegacySql": False
+    }
+    headers = {
+        "Accept": "application/json"
+    }
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        rows = response.json().get("rows", [])
+        return pd.DataFrame([row["f"] for row in rows])
+    else:
+        st.error(f"Error {response.status_code}: {response.text}")
+        return pd.DataFrame()
 
 # Use Streamlit to display the data
 st.write(fetch_data(QUERY))
