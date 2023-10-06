77010f222cc3b58d9204d805568776ca83a50884
Michael <michael.feduk@gmail.com>
2023-10-06 12:30:10 +0000

    update project_id

--- pages/5_DataFrame_Test.py
+++ pages/5_DataFrame_Test.py
@@ -8,7 +8,7 @@ LIMIT 100
 
 # Fetch data from BigQuery
 def fetch_data(query):
-    return pd.read_gbq(query, project_id="your_project_id", dialect="standard", use_bqstorage_api=True)
+    return pd.read_gbq(query, project_id="elegant-tendril-395105", dialect="standard", use_bqstorage_api=True)
 
 # Use Streamlit to display the data
 st.write(fetch_data(QUERY))
